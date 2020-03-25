5b8fda531e6282cf9b69eb9e4d8b3856d06bbaf1
Serhiy Likhanov <slikhan@gmail.com>
2020-03-24 20:10:29 -0400

    tmux plugin system, dir_colors
    1. Setup plugin system for tmux.
    2. Added dir_colors.
    3. Nord colorscheme for tmux.

--- setup.sh
+++ setup.sh
@@ -7,6 +7,17 @@ else
     echo "Configuration folder is already sym-linked."
 fi
 
+# Dircolors
+
+if [ ! -f ~/.dir_colors ]; then
+    echo "Creating symlink to the .dir_colors."
+    ln -s ~/dotconfig/dir_colors ~/.dir_colors
+else
+    echo ".dir_colors is already sym-linked."
+fi
+
+# TMUX
+
 if [ ! -f ~/.tmux.conf ]; then
     echo "Creating symlink to the .tmux.conf."
     ln -s ~/dotconfig/tmux.conf ~/.tmux.conf
